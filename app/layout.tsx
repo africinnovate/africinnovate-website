4191b26e7698dbc64007d12817120bfbdb81e721
Justin Obi <justin@Justins-MacBook-Pro.local>
2024-01-02 08:25:57 +0100

    WIP

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
           <AppHeader />
           {children}
           <AppFooter />
-          {/* <MyApp /> */}
+          <MyApp />
         </main>
       </body>
     </html>
