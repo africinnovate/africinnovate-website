23d0e11fe8d9ec5ecb43fc33912c67c634f236da
cjustinobi <justin@Justins-MacBook-Pro.local>
2024-05-15 11:06:18 +0100

    Eventlayout

--- components/EventLayout.tsx
+++ components/EventLayout.tsx
@@ -5,6 +5,8 @@ import { getEventPosts } from '@/lib/utils'
 import { PostType } from '@/interfaces'
 import EventCard from './ui/EventCard'
 import Head from 'next/head'
+import Link from 'next/link'
+
 interface EventProps {
   data: PostType[]
   // title: string
@@ -23,33 +25,32 @@ const EventLayout = ({ data }: EventProps) => {
     setUpcoming(fEvent);
   } ,[data])
   
-  
 
   return (
     <div className="flex flex-col items-center justify-center mt-[50px]">
       <Head>
-        <meta name="google-adsense-account" content="ca-pub-9738664936744628"/>
+        <meta name="google-adsense-account" content="ca-pub-9738664936744628" />
       </Head>
-    <div
-      className="max-md:text-[26px] text-[40px] font-bold font-['Raleway'] leading-[48px] mb-10"
-      data-aos="zoom-in"
-    >
-      Upcoming Events
+      <div
+        className="max-md:text-[26px] text-[40px] font-bold font-['Raleway'] leading-[48px] mb-10"
+        data-aos="zoom-in"
+      >
+        Upcoming Events
+      </div>
+      <div className="flex w-full  justify-center max-md:grid gap-[100px] ">
+        {upcoming.length > 0 ? (
+          upcoming.map((item) => <EventCard post={item} key={item.sys.id} />)
+        ) : (
+          <div>
+            <Link href="/past-events">
+              <button className=" text-lg font-semibold rounded-3xl  p-4 hover:bg-[#FFBE0B] bg-[#cccce0] font-sans text-[#006] ">
+                View Past Events
+              </button>
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
-    <div className="flex w-full  justify-center max-md:grid gap-[100px] ">
-      {upcoming.length > 0 ?
-        upcoming.map((item) => (
-          <EventCard
-            post={item}
-            key={item.sys.id}
-          />
-        )): 
-        <div>
-          <h1 className='text-white text-3xl line-through text-center font-semibold'>No Events</h1>
-        </div> 
-        }
-    </div>
-  </div>
   )
 }
 
